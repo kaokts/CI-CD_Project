84d3fa125650997bf7507a4816db7eb03c4951d6
kaokts <kaokunza@gmail.com>
2021-09-12 21:26:16 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.get("/", (req, res) =>{
+    res.json({result:"ok", data:[1,2,3,4,5]})
+})
+
+app.listen(PORT, () =>{
+    console.log('Server is running.'+'${PORT}')
+})
