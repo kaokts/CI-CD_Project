becd16cf9a7c3acd86078607c356e51c7744fadb
mostxd <73267225+Nawee35@users.noreply.github.com>
2021-12-20 22:25:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
-cd /etc/yum.repos.d/
-sudo touch mongodb-org-5.0.repo
-cat >mongodb-org-5.0.repo<<EOL
-[mongodb-org-5.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
-EOL
-cd /home/ec2-user/
-sudo yum install -y mongodb-org
-sudo systemctl start mongod
 cd project360/
 npm install
 cd seed/
